c33872b7089d665b9ad612162601e9e6cb2c417f
Felipe Araujo <felipe.ar@pm.me>
2020-09-27 09:59:17 -0300

    endereco

--- src/main/kotlin/br/com/empresa/financeiro/endereco/Endereco.kt
+++ src/main/kotlin/br/com/empresa/financeiro/endereco/Endereco.kt
@@ -1,4 +1,31 @@
 package br.com.empresa.financeiro.endereco
 
-class Endereco {
+import com.google.gson.annotations.Expose
+
+open class Endereco(
+    @Expose
+    var ruaEndereco: String? = null,
+    @Expose
+    var numeroEndereco: String? = null,
+    @Expose
+    var complementoEndereco: String? = null,
+    @Expose
+    var cepEndereco: String? = null,
+    @Expose
+    var cidadeEndereco: String? = null,
+    @Expose
+    var estadoEndereco: String? = null
+) {
+
+    fun vEndereco():Boolean{
+        val nulo = listOf(
+            ruaEndereco,
+            numeroEndereco,
+            cepEndereco,
+            cidadeEndereco,
+            estadoEndereco
+        ).any{it == null}
+        return !nulo
+    }
+
 }
