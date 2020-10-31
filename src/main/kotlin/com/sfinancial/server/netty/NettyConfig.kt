ef2f3f74ec2592b35b30ba5b2c70e5bad058068f
Felipe Araujo <felipe.ar@pm.me>
2020-10-31 12:09:47 -0300

    removing sensitive data

---
+++ src/main/kotlin/com/sfinancial/server/netty/NettyConfig.kt
@@ -0,0 +1,44 @@
+package com.sfinancial.server.netty
+
+import com.sfinancial.notification.exception.FileNotFound
+import java.io.File
+import java.io.FileNotFoundException
+
+class NettyConfig {
+    fun getConnectionString(): String {
+        try {
+            return File("ConnectionString.txt").readLines()[0]
+        }catch (e: FileNotFoundException){
+            throw FileNotFound("Arquivo nao encontrado!")
+        }catch (e: Exception){
+            throw e
+        }
+    }
+    fun getDatabaseName(): String{
+        try {
+            return File("databseName.txt").readLines()[0]
+        }catch (e: FileNotFoundException){
+            throw FileNotFound("Arquivo nao encontrado!")
+        }catch (e: Exception){
+            throw e
+        }
+    }
+    fun getSecretJwt():String{
+        try {
+            return File("secretJwt.txt").readLines()[0]
+        }catch (e: FileNotFoundException){
+            throw FileNotFound("Arquivo nao encontrado!")
+        }catch (e: Exception){
+            throw e
+        }
+    }
+    fun getIssuer(): String{
+        try {
+            return File("Issuer.txt").readLines()[0]
+        }catch (e: FileNotFoundException){
+            throw FileNotFound("Arquivo nao encontrado!")
+        }catch (e: Exception){
+            throw e
+        }
+    }
+}
