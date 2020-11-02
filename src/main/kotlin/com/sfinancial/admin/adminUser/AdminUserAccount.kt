c3f058f30f7829f652ce6e39d801bbd259f80e1a
Felipe Araujo <felipe.ar@pm.me>
2020-11-02 13:58:48 -0300

    Changing AdminUser

--- src/main/kotlin/com/sfinancial/admin/adminUser/AdminUserAccount.kt
+++ src/main/kotlin/com/sfinancial/admin/adminUser/AdminUserAccount.kt
@@ -1,11 +1,12 @@
 package com.sfinancial.admin.adminUser
 
 
+import com.sfinancial.Account.UserAccount
 import com.sfinancial.database.DBInterface
 import com.sfinancial.group.GroupInterface
 
 class AdminUserAccount(
-        private val groupInterface: GroupInterface,
+        private val userAccount: UserAccount,
         private val dbInterface: DBInterface
 ) {
     fun register(){
