100b069c2cace2980faceba11d0af35bdb6ecf80
Felipe Araujo <felipe.ar@pm.me>
2020-11-06 20:57:35 -0300

    Jwt Config

--- src/main/kotlin/com/sfinancial/config/jwtConfig/EnvJwtConfig.kt
+++ src/main/kotlin/com/sfinancial/config/jwtConfig/EnvJwtConfig.kt
@@ -7,29 +7,17 @@ import java.io.FileNotFoundException
 class EnvJwtConfig: JwtConfigInterface {
     override fun getSecretJwt():String{
         return try {
-            File("secretJwt.txt").readLines()[0]
-        }catch (e: FileNotFoundException){
-            try {
                 System.getenv("sJwt")
             }catch (e: Exception){
                 throw e
             }
-        }catch (e: Exception){
-            throw e
-        }
     }
 
     override fun getIssuer(): String{
         return try {
-            File("connectionIssuer.txt").readLines()[0]
-        }catch (e: FileNotFoundException){
-            try {
                 System.getenv("cIssuer")
             }catch (e: Exception){
                 throw e
             }
-        }catch (e: Exception){
-            throw e
-        }
     }
 }
