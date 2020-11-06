fa5ddbf1584408c0c2a17fc4a981170bdb265428
Felipe Araujo <felipe.ar@pm.me>
2020-11-06 20:59:46 -0300

    hashid Config

--- src/main/kotlin/com/sfinancial/config/hashidConfig/EnvHashIdConfig.kt
+++ src/main/kotlin/com/sfinancial/config/hashidConfig/EnvHashIdConfig.kt
@@ -7,15 +7,9 @@ import java.io.FileNotFoundException
 class EnvHashIdConfig: HashIdConfigInterface {
     override fun getSecretHashid():String{
         return try {
-            File("secretHashid.txt").readLines()[0]
-        }catch (e: FileNotFoundException) {
-            try {
                 System.getenv("sHashid")
             }catch (e: Exception){
                 throw e
             }
-        }catch (e: Exception){
-            throw e
-        }
     }
 }
