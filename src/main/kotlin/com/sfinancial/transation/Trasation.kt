5b77d361060fee5925ea214e8f56080e5863c08c
Felipe Augusto <felipeaugustossouza@gmail.com>
2020-11-09 15:15:25 -0300

    Transation

--- src/main/kotlin/com/sfinancial/transation/Trasation.kt
+++ src/main/kotlin/com/sfinancial/transation/Trasation.kt
@@ -1,4 +1,19 @@
 package com.sfinancial.transation
 
-class Trasation {
+open class Trasation(
+        var idTransacao: String?= null,
+        var data: String? = null,
+        var valor: Double? = null,
+        var local: String? = null,
+) {
+    fun verifier(): Boolean{
+        val nulo = listOf(
+                idTransacao,
+                data,
+                valor,
+                local
+        ).any { it == null }
+
+        return !nulo
+    }
 }
